e1915ad97806fb50ae2c7e55310bebba8681656e
Oleksiy Oletsky <oletsky@ukr.net>
2022-06-14 16:55:31 +0300

    Changed message

--- src/demopackage/DemoClass.java
+++ src/demopackage/DemoClass.java
@@ -2,6 +2,6 @@ package demopackage;
 
 public class DemoClass {
     public static void main(String[] args) {
-        System.out.println("Hello, word of version controls!");
+        System.out.println("Miaoo!");
     }
 }
